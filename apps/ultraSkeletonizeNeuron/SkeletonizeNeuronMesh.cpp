8763f8979b6d204585b3131d405a217548258fb1
Marwan Abdellah <abdellah.marwan@gmail.com>
2024-04-08 07:06:38 +0200

    Changing the order to export the soma before the spine segmentation.

--- apps/ultraSkeletonizeNeuron/SkeletonizeNeuronMesh.cpp
+++ apps/ultraSkeletonizeNeuron/SkeletonizeNeuronMesh.cpp
@@ -92,13 +92,15 @@ void run(int argc , const char** argv)
     // Run the skeletonization operations
     runNeuronSkeletonizationOperations(options, skeletonizer);
 
+    // Run the soma export operations
+    runSomaExportOperations(options, skeletonizer);
+
     // Run the spine segmentation operations
     runSpineSegmentationOperations(options, skeletonizer, inputNeuronMesh);
 
     // Run cross-sectional variations along the arbors
 
-    // Run the soma export operations
-    runSomaExportOperations(options, skeletonizer);
+
 }
 
 }
